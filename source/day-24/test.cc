8d206632522c596d262a43b99638ebf7f1051a51
raphael meyer <raphael.akira.meyer@gmail.com>
2020-12-23 16:35:07 +0100

    templates for day 24 and 25

---
+++ source/day-24/test.cc
@@ -0,0 +1,8 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+
+#include "xxx.h"
+
+TEST_CASE("Example - Part I") { REQUIRE(true); }
+
+TEST_CASE("Example - Part II") { REQUIRE(true); }
---
+++ source/day-25/main.cc
@@ -0,0 +1,8 @@
+#include "xxx.h"
+
+#include <iostream>
+
+int main() {
+  std::cout << "Part I : " << 0 << "\n";
+  std::cout << "Part II : " << 0 << "\n";
+}
